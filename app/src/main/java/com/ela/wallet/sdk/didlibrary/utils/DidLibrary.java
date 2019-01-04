2ec244cbf31873f40588a4c25e5fbc4f98bd02cf
maxiaoxu <maxiaoxu2017@163.com>
2019-01-04 13:59:30 +0800

    修复积分转账不成功的问题

--- app/src/main/java/com/ela/wallet/sdk/didlibrary/utils/DidLibrary.java
+++ app/src/main/java/com/ela/wallet/sdk/didlibrary/utils/DidLibrary.java
@@ -440,10 +440,10 @@ public class DidLibrary {
         long amount = Math.round(Float.parseFloat(aParam) * 100000000) ;
         String fromAddress = Utilty.getPreference(Constants.SP_KEY_DID_ADDRESS, "");
 
-        //for test
-        toAddress = fromAddress;
-        fromAddress = "ESs1jakyQjxBvEgwqEGxtceastbPAR1UJ4";
-        mPrivateKey = "840d6c631e3d612aa624dae2d7f6d354e58135a7a6cb16ed6dd264b7d104aae7";
+//        //for test
+//        toAddress = fromAddress;
+//        fromAddress = "ESs1jakyQjxBvEgwqEGxtceastbPAR1UJ4";
+//        mPrivateKey = "840d6c631e3d612aa624dae2d7f6d354e58135a7a6cb16ed6dd264b7d104aae7";
 
         String param = String.format("{\"inputs\":[\"%s\"],\"outputs\":[{\"addr\":\"%s\", \"amt\":%d}]}", fromAddress, toAddress, amount);
         HttpRequest.sendRequestWithHttpURLConnection(Urls.SERVER_DID + Urls.DID_CTX, param, new HttpRequest.HttpCallbackListener() {
